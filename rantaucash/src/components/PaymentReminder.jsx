3d972a7cdb89e0f48f561cff41d882180fc3a6d2
ViktorPratama <114640656+ViktorPratama@users.noreply.github.com>
2024-12-12 11:00:56 +0900

    Update PaymentReminder.jsx

--- rantaucash/src/components/PaymentReminder.jsx
+++ rantaucash/src/components/PaymentReminder.jsx
@@ -7,7 +7,7 @@ const PaymentReminder = () => {
 
   const handleSetReminder = async () => {
     try {
-      await axios.post('http://localhost:5000/api/payments/reminder', { date: reminderDate });
+      await axios.post('https://backend-o47f.vercel.app/api/payments/reminder', { date: reminderDate });
       setMessage('Reminder set successfully!');
     } catch (error) {
       setMessage('Failed to set reminder.');
