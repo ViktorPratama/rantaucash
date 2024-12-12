92a70565b1334f5037a83d41872a411080fd828d
ViktorPratama <114640656+ViktorPratama@users.noreply.github.com>
2024-12-12 10:42:36 +0900

    Update Login.jsx

--- rantaucash/src/pages/Login.jsx
+++ rantaucash/src/pages/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/api/users/login', {
+      const response = await axios.post('https://backend-o47f.vercel.app//api/users/login', {
         email,
         password,
       });
